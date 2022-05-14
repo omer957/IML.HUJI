142c8699973480eeafa19d87d679ff7c3b1d9209
omer957 <omer.goldkorn@gmail.com>
2022-05-14 17:50:02 +0300

    ex 4- all most done!

--- IMLearn/learners/classifiers/decision_stump.py
+++ IMLearn/learners/classifiers/decision_stump.py
@@ -41,7 +41,19 @@ class DecisionStump(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        raise NotImplementedError()
+
+        n_features = X.shape[1]
+        X_t = X.T
+        best_err = 2
+
+        for sign, f_index in product([-1, 1], range(n_features)):
+            curr_feature = X_t[f_index]
+            f_TH, curr_ER = self._find_threshold(curr_feature, y, sign)
+            if best_err > curr_ER:
+                best_err = curr_ER
+                self.sign_ = sign
+                self.j_ = f_index
+                self.threshold_ = f_TH
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -65,7 +77,7 @@ class DecisionStump(BaseEstimator):
         Feature values strictly below threshold are predicted as `-sign` whereas values which equal
         to or above the threshold are predicted as `sign`
         """
-        raise NotImplementedError()
+        return np.where(X[:, self.j_] >= self.threshold_, self.sign_, -self.sign_)
 
     def _find_threshold(self, values: np.ndarray, labels: np.ndarray, sign: int) -> Tuple[float, float]:
         """
@@ -100,12 +112,8 @@ class DecisionStump(BaseEstimator):
         sorted_indexes = values.argsort()
         values_sorted = values[sorted_indexes]
         labels_sorted = labels[sorted_indexes]
-        # purpose - find the index i where all the next index-s, including i, are with the minimum values that are below TH
 
-        # vector that marks where the labels are under TH
-        under_TH = (np.where(labels_sorted >= 0, 1, -1) != sign).astype(int)
-        # amount of labels under TH
-        n_under_TH = np.sum(under_TH)
+        # purpose - find the index i where all the next index-s, including i, are with the minimum values that are below TH
         # reverse labels with opposite labels:
         #  - reverse in order to see in the opposite side when cumsum
         #  - multiply - for calculate the balance of under TH against g/e TH (favour for g/e TH)
@@ -113,12 +121,17 @@ class DecisionStump(BaseEstimator):
         prep1 = (labels_sorted * sign)[::-1]
         prep2 = np.cumsum(prep1)  # calculate the balance from the end to the start
         TH_balance = prep2[::-1]  # reverse ->  the balance of each index (including) until the end = number of good labeling until the index.
-        final_TH_balance = n_under_TH - TH_balance  # calculate amount of errors in total for each case /<- not necessary (could argmax on TH_balance)
-        min_val_ind = np.argmin(final_TH_balance)
-        y_pred = np.concatenate((-sign * np.ones(min_val_ind), sign * np.ones(labels_sorted.shape[0] - min_val_ind)))
-        loss = 1 - np.mean((labels_sorted - y_pred == 0).astype(int))
 
-        return values_sorted[min_val_ind], loss
+        min_val_ind = np.argmax(TH_balance)
+
+        y_pred = np.concatenate((-sign * np.ones(min_val_ind), sign * np.ones(labels_sorted.shape[0] - min_val_ind)))
+        # multiply with abs('labels_sorted') in order to make weighted loss
+        loss = np.sum(np.abs(labels_sorted) * (np.where(labels_sorted >= 0, 1, -1) != y_pred).astype(int))
+
+        if min_val_ind > 0:
+            return values_sorted[min_val_ind], loss
+        else:
+            return -np.inf, loss
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -140,6 +153,4 @@ class DecisionStump(BaseEstimator):
 
         y_pred = self.predict(X)
         y_true = np.where(y >= 0, 1, -1)
-        return misclassification_error(y_true, y_pred)
-
-        raise NotImplementedError()
+        return misclassification_error(y_true=y_true, y_pred=y_pred)
--- exercises/adaboost_scenario.py
+++ exercises/adaboost_scenario.py
@@ -1,10 +1,11 @@
 import numpy as np
 from typing import Tuple
-from IMLearn.learners.metalearners.adaboost import AdaBoost
+from IMLearn.metalearners.adaboost import AdaBoost
 from IMLearn.learners.classifiers import DecisionStump
 from utils import *
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from IMLearn.metrics.loss_functions import accuracy
 
 
 def generate_data(n: int, noise_ratio: float) -> Tuple[np.ndarray, np.ndarray]:
@@ -39,23 +40,120 @@ def generate_data(n: int, noise_ratio: float) -> Tuple[np.ndarray, np.ndarray]:
 
 
 def fit_and_evaluate_adaboost(noise, n_learners=250, train_size=5000, test_size=500):
-    (train_X, train_y), (test_X, test_y) = generate_data(train_size, noise), generate_data(test_size, noise)
+    (X_train, Y_train), (X_test, Y_test) = generate_data(train_size, noise), generate_data(test_size, noise)
 
     # Question 1: Train- and test errors of AdaBoost in noiseless case
-    raise NotImplementedError()
+
+    print("Q1 - start")
+    adaboost_model = AdaBoost(wl=DecisionStump, iterations=n_learners)
+    adaboost_model.fit(X_train, Y_train)
+    print('\ngenerating graph...')
+    fig1 = go.Figure(
+        data=[
+            go.Scatter(
+                x=np.linspace(1, n_learners, n_learners),
+                y=list(map(lambda n: adaboost_model.partial_loss(X_train, Y_train, int(n)), np.linspace(1, n_learners, n_learners))),
+                mode='markers+lines',
+                name="training data",
+                marker=dict(size=5, opacity=0.6),
+                line=dict(width=3)
+            ),
+            go.Scatter(
+                x=np.linspace(1, n_learners, n_learners),
+                y=list(map(lambda n: adaboost_model.partial_loss(X_test, Y_test, int(n)), np.linspace(1, n_learners, n_learners))),
+                mode='markers+lines',
+                name="test data",
+                marker=dict(size=5, opacity=0.6),
+                line=dict(width=3)
+            )
+        ],
+        layout=go.Layout(
+            title=f"Loss as function of num of learnesr; with {noise} noise.",
+            xaxis_title={'text': "$\\text{Num of learners}$"},
+            yaxis_title={'text': "$\\text{Misclassification error}$"}
+        )
+    )
+
+    fig1.write_image("./q1.png")
+    print("\nQ1 - end ************ \n\n")
 
     # Question 2: Plotting decision surfaces
+    print("Q2 - start")
+
     T = [5, 50, 100, 250]
-    lims = np.array([np.r_[train_X, test_X].min(axis=0), np.r_[train_X, test_X].max(axis=0)]).T + np.array([-.1, .1])
-    raise NotImplementedError()
+    lims = np.array([np.r_[X_train, X_test].min(axis=0), np.r_[X_train, X_test].max(axis=0)]).T + np.array([-.1, .1])
+    symbols = np.array(["circle", "x"])
+
+    print('\ngenerating graph...')
+    fig2 = make_subplots(rows=2,
+                         cols=2,
+                         subplot_titles=[f"Decision boundary for ensemble with {t} weak learners" for t in T],
+                         horizontal_spacing=0.1,
+                         vertical_spacing=0.1)
+
+    for i, t in enumerate(T):
+        fig2.add_traces([decision_surface(lambda x: adaboost_model.partial_predict(x, t), lims[0], lims[1], showscale=False),
+                         go.Scatter(x=X_test[:, 0], y=X_test[:, 1], mode="markers", showlegend=False,
+                                    marker=dict(color=Y_test,
+                                                symbol='diamond',
+                                                colorscale=[custom[0], custom[-1]],
+                                                line=dict(color="black", width=1)))],
+                        rows=(i // 2) + 1, cols=(i % 2) + 1)
+
+    fig2.update_layout(title_text=f"decision boundary obtained by using the  weighted ensembles of different sizes; with {noise} noise.", font_size=15,
+                       margin=dict(t=100))
+
+    fig2.write_image("./q2.png")
+    print("\nQ2 - end ************ \n\n")
 
     # Question 3: Decision surface of best performing ensemble
-    raise NotImplementedError()
+    print("Q3 - start")
+    min_ind = np.argmin(np.array([adaboost_model.partial_loss(X_test, Y_test, t) for t in range(1, 251)]))
+    ensemble_size = min_ind + 1
+
+    acc = accuracy(Y_test, adaboost_model.partial_predict(X_test, ensemble_size))
+    print('\ngenerating graph...')
+    fig3 = go.Figure(
+        [decision_surface(lambda x: adaboost_model.partial_predict(x, ensemble_size), lims[0], lims[1], showscale=False),
+         go.Scatter(x=X_test[:, 0], y=X_test[:, 1], mode="markers", showlegend=False,
+                    marker=dict(color=Y_test,
+                                symbol='diamond',
+                                colorscale=[custom[0], custom[-1]],
+                                line=dict(color="black", width=1)))],
+        layout=go.Layout(
+            title=f"the ensemble that achieves the lowest test error is ensemble of size {ensemble_size}, with accuracy of: {acc}; with {noise} noise.",
+            font_size=15
+        )
+    )
+
+    fig3.write_image("./q3.png")
+    print("\nQ3 - end ************ \n\n")
 
     # Question 4: Decision surface with weighted samples
-    raise NotImplementedError()
+    print("Q4 - start")
+    D_normal = 5 * adaboost_model.D_ / np.max(adaboost_model.D_)
+    print('\ngenerating graph...')
+    fig4 = go.Figure(
+        [decision_surface(adaboost_model.predict, lims[0], lims[1], showscale=False),
+         go.Scatter(x=X_train[:, 0], y=X_train[:, 1], mode="markers", showlegend=False,
+                    marker=dict(color=Y_test,
+                                symbol='diamond',
+                                size=D_normal,
+                                colorscale=[custom[0], custom[-1]],
+                                line=dict(color="black", width=1)))],
+        layout=go.Layout(
+            title=f"decision boundary obtained by using the weighted ensembles of size 250; with {noise} noise.",
+            font_size=15
+        )
+    )
+    fig4.write_image("./q4.png")
+    print("\nQ4 - end ************ \n\n")
+    print('*******************************************************************')
 
 
 if __name__ == '__main__':
     np.random.seed(0)
-    raise NotImplementedError()
+    fit_and_evaluate_adaboost(noise=0)
+    # fit_and_evaluate_adaboost(noise=0.4)  # q-5
+
+
